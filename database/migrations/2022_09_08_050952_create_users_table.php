a056023a7beb7d5687680da67adebf18bf6f326a
Nakaya <eniehack@outlook.jp>
2022-09-08 14:50:25 +0900

    first commit

---
+++ database/migrations/2022_09_08_050952_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->char('ulid',26);
+            $table->string('user_id',15);
+            $table->string('name',20);
+            $table->char('password',97);
+            $table->boolean('is_admin')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
