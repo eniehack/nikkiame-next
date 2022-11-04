533c17be38fa6b127ff42a02b0340272b546188d
kisuisou <irwinlex@yahoo.co.jp>
2022-11-04 23:01:09 +0900

    SignupControllerの実装

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 use App\Models\Post;
-
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Hash;
 use Ulid\Ulid;
 
@@ -21,40 +20,8 @@ use Ulid\Ulid;
 |
 */
 
-Route::get('/signup', function () {
-    return view('signup');
-});
-
-// name: required, max(20)
-// pass: required, minlength
-// uid: user_id, unique, max(15), min(1)
-Route::post('/signup', function(Request $request) {
-
-    $validator = Validator::make($request->all(), [
-        "name" => ["required","max:20"],
-        "uid" => ["required","max:15","min:1","regex:/^[0-9a-z._]+$/","unique:App\Models\User,user_id"],
-        "pass" => ["required","min:8"]
-    ]);
-
-    if ($validator->fails()) {
-        return redirect('/signup')->withErrors($validator)->withInput();
-    }
-
-    $name=$request->string('name');
-    $pass=$request->string('pass');
-    $uid=$request->string('uid');
-
-    $user = new User;
-
-    $user->name = $name;
-    $user->password = Hash::make($pass);
-    //$user->password = password_hash($pass, PASSWORD_ARGON2ID, ['memory_cost' => 65536, 'time_cost' => 4, 'threads' => 1]);
-    $user->user_id = $uid;
-    $user->ulid = (string) Ulid::generate();
-    $user->save();
-
-    return redirect('/signin');
-});
+Route::get('/signup',[SignupController::class,'get'] );
+Route::post('/signup',[SignupController::class,'post'] );
 
 Route::get('/signin',function(){
     return view('signin');
