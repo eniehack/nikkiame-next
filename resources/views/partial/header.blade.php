180c237bf1569c070ccbf6fb66cbf6a12f464b13
kisuisou <irwinlex@yahoo.co.jp>
2023-09-26 23:09:51 +0900

    headerの表示内容をログイン状態に応じて変更

--- resources/views/partial/header.blade.php
+++ resources/views/partial/header.blade.php
@@ -7,15 +7,21 @@
             <i class="material-icons">menu</i>
         </a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="/signin">Signin</a></li>
-            <li><a href="/signup">Signup</a></li>
-            <li><a href="{{route('posts.create')}}">新規投稿</a></li>
+            @if ( session()->has('name') )
+                <li><a href="{{ route('user.profile', ['user' => session('uid')]) }}">my page</a></li>
+                <li><a href="{{ route('posts.create') }}">新規投稿</a></li>
+            @else
+                <li><a href="{{ route('signin.get') }}">Signin</a></li>
+            @endif
         </ul>
     </div>
 </nav>
 
 <ul class="sidenav" id="mobile-demo">
-  <li><a href="/signin">Signin</a></li>
-  <li><a href="/signup">Signup</a></li>
-  <li><a href="{{route('posts.create')}}">新規投稿</a></li>
+    @if ( session()->has('name') )
+        <li><a href="{{ route('user.profile', ['user' => session('uid')]) }}">my page</a></li>
+        <li><a href="{{ route('posts.create') }}">新規投稿</a></li>
+    @else
+        <li><a href="{{ route('signin.get') }}">Signin</a></li>
+    @endif
 </ul>
