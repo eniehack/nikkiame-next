2da007016e6979e201d02a1dde2814e38f830757
kisuisou <irwinlex@yahoo.co.jp>
2023-09-26 23:25:02 +0900

    mypageの表記を@user_idに変更

--- resources/views/partial/header.blade.php
+++ resources/views/partial/header.blade.php
@@ -8,7 +8,7 @@
         </a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
             @if ( session()->has('name') )
-                <li><a href="{{ route('user.profile', ['user' => session('uid')]) }}">my page</a></li>
+                <li><a href="{{ route('user.profile', ['user' => session('uid')]) }}">{{ "@".session('uid') }} </a></li>
                 <li><a href="{{ route('posts.create') }}">新規投稿</a></li>
             @else
                 <li><a href="{{ route('signin.get') }}">Signin</a></li>
@@ -19,7 +19,7 @@
 
 <ul class="sidenav" id="mobile-demo">
     @if ( session()->has('name') )
-        <li><a href="{{ route('user.profile', ['user' => session('uid')]) }}">my page</a></li>
+        <li><a href="{{ route('user.profile', ['user' => session('uid')]) }}">{{ "@".session('uid') }} </a></li>
         <li><a href="{{ route('posts.create') }}">新規投稿</a></li>
     @else
         <li><a href="{{ route('signin.get') }}">Signin</a></li>
